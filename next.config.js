5c60489fb949ac07f885385a41de84ee0d5e83af
luigi009 <luigifanaro@hotmail.com>
2021-04-27 10:38:01 -0300

    Update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    basePath: '/netflix-clone',
-    assetPrefix: '/netflix-clone'
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
