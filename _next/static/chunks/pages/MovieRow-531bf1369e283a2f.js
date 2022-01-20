d65c1db7febd5a9a3d86d50d1782c63eca625ec7
luigi009 <luigifanaro@hotmail.com>
2022-01-20 14:21:25 +0000

    Deploying to gh-pages from @ luigi009/netflix-clone@94716f692de7f04cf83c684ae90938869bef6aa9 🚀

---
+++ _next/static/chunks/pages/MovieRow-531bf1369e283a2f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[387],{9091:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/MovieRow",function(){return u(7656)}])}},function(n){n.O(0,[772,259,924,553,590,46,656,774,888,179],(function(){return _=9091,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
